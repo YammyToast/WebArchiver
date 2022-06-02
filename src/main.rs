af6602e84998e10296bab0b516319d5b314663a0
YammyToast <cyanjamesmail@gmail.com>
2022-06-02 18:09:41 +0100

    Database Progress - 1

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,52 @@
 extern crate diesel;
 extern crate dotenv;
 
-use diesel::{pg::PgConnection, Connection};
+use diesel::{pg::PgConnection, Connection, ConnectionError};
 use std::env;
 
 
+use crate::schema::sitepages::dsl::*;
+use crate::model::Sitepage;
+
+
 mod schema;
-//mod model;
+mod model;
 
 fn main() {
-    dotenv::from_filename(".env").expect("Error locating env file");
-    let data_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL var not found or invalid.");
-    let connection = PgConnection::establish(&data_url)
-        .expect("Database connection could not be established.");
+
+
+
+    // Matches a connection case and panics if no connection is established.
+    let connection = match establish_connection() {
+        None => panic!("No Connection Established"),
+        Some(val) => val  
+    };
+
+    let results = sitepages.filter() 
+    //sitepages.limit(2).load::<pageid>(&connection).expect("Err");
     
-    
+
+
     
     
 }
 
+
+fn establish_connection() -> Option<PgConnection> {
+    // Establish dotenv file stream. 
+    // Standard ::dotenv call loaded sys env's so this is a verbose workaround.
+    dotenv::from_filename(".env").expect("Error locating env file");
+
+    // Parse env stream for DATABASE_URL var.
+    let data_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL var not found or invalid.");
+
+    // Create connection struct using database url.
+    let connection = PgConnection::establish(&data_url);
+
+    // If there is a connection, return the connection struct, otherwise return none.
+    match connection {
+        Ok(val) => Some(val),
+        _ => None
+    }
+}
